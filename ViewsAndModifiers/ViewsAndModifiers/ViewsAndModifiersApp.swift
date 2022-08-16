8f799b2a458c8823398bfdbc85920103031adf21
luncenok <idziejczak3@gmail.com>
2022-08-16 15:44:11 +0200

    project 3 challenge

---
+++ ViewsAndModifiers/ViewsAndModifiers/ViewsAndModifiersApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewsAndModifiersApp.swift
+//  ViewsAndModifiers
+//
+//  Created by Mateusz Idziejczak on 15/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct ViewsAndModifiersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
