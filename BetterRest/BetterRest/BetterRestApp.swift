dedadb74ee37680e49eb614a954825832d8c415e
luncenok <idziejczak3@gmail.com>
2022-08-17 10:41:02 +0200

    Project 4

---
+++ BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Mateusz Idziejczak on 16/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
