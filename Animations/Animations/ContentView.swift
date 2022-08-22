6f0c142de0a3c87ac02268f09cce2aac58136869
luncenok <idziejczak3@gmail.com>
2022-08-22 13:41:52 +0200

    day 33

--- Animations/Animations/ContentView.swift
+++ Animations/Animations/ContentView.swift
@@ -7,30 +7,45 @@
 
 import SwiftUI
 
+struct CornerRotateModifier: ViewModifier {
+    let amount: Double
+    let anchor: UnitPoint
+    
+    func body(content: Content) -> some View {
+        content
+            .rotationEffect(.degrees(amount), anchor: anchor)
+            .clipped()
+    }
+}
+
+extension AnyTransition {
+    static var pivot: AnyTransition {
+        .modifier(
+            active: CornerRotateModifier(amount: -90, anchor: .topLeading),
+            identity: CornerRotateModifier(amount: 0, anchor: .topLeading)
+        )
+    }
+}
+
 struct ContentView: View {
-    @State private var animationAmount = 0.0
+    @State private var isShowingRed = false
+    
     var body: some View {
-        VStack {
-            //            Stepper("Scale amount", value: $animationAmount.animation(), in: 1...10)
-            //            Spacer()
-            Button("Tap me") {
-                withAnimation {
-                    animationAmount += 360
-                }
+        ZStack {
+            Rectangle()
+                .fill(.blue)
+                .frame(width: 200, height: 200)
+            if isShowingRed {
+                Rectangle()
+                    .fill(.red)
+                    .frame(width: 200, height: 200)
+                    .transition(.pivot)
             }
-            .padding(50)
-            .background(.red)
-            .foregroundColor(.white)
-            .clipShape(Circle())
-            .rotation3DEffect(.degrees(animationAmount), axis: (x: 0, y: 0.3, z: 0))
-            //            .overlay(
-            //                Circle()
-            //                    .stroke(.red)
-            //                    .scaleEffect(animationAmount)
-            //                    .opacity(2-animationAmount)
-            //                    .animation(.easeInOut(duration: 1).repeatForever(autoreverses: false), value: animationAmount)
-            //            )
-            //            .scaleEffect(animationAmount)
+        }
+        .onTapGesture {
+            withAnimation {
+                isShowingRed.toggle()
+            }
         }
     }
 }
