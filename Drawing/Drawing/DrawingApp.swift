65a6278c3c57f097d138fe5e1e4632dc7d07331a
luncenok <idziejczak3@gmail.com>
2022-09-03 21:25:41 +0200

    day 43

---
+++ Drawing/Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by Mateusz Idziejczak on 03/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
