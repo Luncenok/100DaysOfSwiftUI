132c5503a2f51ffd1f66458f64d68c797781511a
luncenok <idziejczak3@gmail.com>
2022-09-05 12:06:28 +0200

    day 45

--- Drawing/Drawing/ContentView.swift
+++ Drawing/Drawing/ContentView.swift
@@ -103,15 +103,111 @@ struct ColorCyclingCircle: View {
     }
 }
 
+struct Trapezoid: Shape {
+    var insetAmount: Double
+    
+    var animatableData: Double {
+        get { insetAmount }
+        set { insetAmount = newValue }
+    }
+    
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        path.move(to: CGPoint(x: 0, y: rect.maxY))
+        path.addLine(to: CGPoint(x: insetAmount, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.maxX - insetAmount, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: 0, y: rect.maxY))
+        return path
+    }
+}
+
+struct Checkerboard: Shape {
+    var rows: Int
+    var columns: Int
+    
+    var animatableData: AnimatablePair<Double, Double> {
+        get {
+            AnimatablePair(Double(rows), Double(columns))
+        }
+        set {
+            rows = Int(newValue.first)
+            columns = Int(newValue.second)
+        }
+    }
+    
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        
+        let rowSize = rect.height / Double(rows)
+        let columnSize = rect.width / Double(columns)
+        
+        for row in 0..<rows {
+            for column in 0..<columns {
+                if (row + column).isMultiple(of: 2) {
+                    let startX = columnSize * Double(column)
+                    let startY = rowSize * Double(row)
+                    
+                    let rect = CGRect(x: startX, y: startY, width: columnSize, height: rowSize)
+                    path.addRect(rect)
+                }
+            }
+        }
+        return path
+    }
+}
+
 struct ContentView: View {
     @State private var petalOffset = -50.0
     @State private var petalWidth = 50.0
     @State private var scale = 0.2
     @State private var colorCycle = 0.0
+    @State private var amount = 0.7
+    @State private var insetAmount = 50.0
+    @State private var rows = 4
+    @State private var columns = 4
     
     var body: some View {
         ScrollView {
             VStack {
+                VStack {
+                    Checkerboard(rows: rows, columns: columns)
+                        .fill(.cyan)
+                        .frame(width: 300, height: 300)
+                        .onTapGesture {
+                            withAnimation(.linear(duration: 3)) {
+                                rows = 8
+                                columns = 16
+                            }
+                        }
+                    Trapezoid(insetAmount: insetAmount)
+                        .fill(.yellow)
+                        .frame(width: 200, height: 100)
+                        .padding()
+                        .onTapGesture {
+                            withAnimation {
+                                insetAmount = Double.random(in: 10...90)
+                            }
+                        }
+                }
+                ZStack {
+                    Circle()
+                        .fill(.red)
+                        .frame(width: 200 * amount)
+                        .offset(x: -50, y: -80)
+                        .blendMode(.screen)
+                    Circle()
+                        .fill(.green)
+                        .frame(width: 200 * amount)
+                        .offset(x: 50, y: -80)
+                        .blendMode(.screen)
+                    Circle()
+                        .fill(.blue)
+                        .frame(width: 200 * amount)
+                        .blendMode(.screen)
+                }
+                .frame(width: 300, height: 300)
+                Slider(value: $amount)
                 ColorCyclingCircle(amount: colorCycle)
                     .frame(width: 300, height: 300)
                 Slider(value: $colorCycle)
@@ -144,6 +240,9 @@ struct ContentView: View {
                     .border(ImagePaint(image: Image("armstrong"), scale: scale), width: 50)
                 Slider(value: $scale, in: 0.0...1.0)
             }
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background(.black)
+            .ignoresSafeArea()
         }
     }
 }
